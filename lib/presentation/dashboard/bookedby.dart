a794bef211e845420fa7d65b410f43e0bfa99a2a
Abraham Kiplagat <kurerelagat01@.com>
2024-06-22 09:30:13 +0300

    reset3

--- lib/presentation/dashboard/bookedby.dart
+++ lib/presentation/dashboard/bookedby.dart
@@ -1,85 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:hotel/presentation/dashboard/no_bookings.dart';
-import 'package:hotel/providers/bookings_provider.dart';
-import 'package:hotel/domain/services/mpesa_service.dart';
-import 'package:hotel/domain/models/booking_models.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BookingDisplayScreen extends StatelessWidget {
-  final MpesaService mpesaService = MpesaService();
-
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => BookingsProvider()..fetchBookings(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Your Bookings'),
-        ),
-        backgroundColor: Colors.purple[100],
-        body: Consumer<BookingsProvider>(
-          builder: (context, bookingsProvider, child) {
-            if (bookingsProvider.isLoading) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            if (bookingsProvider.error != null) {
-              return Center(
-                child: Text(bookingsProvider.error!),
-              );
-            }
-
-            if (bookingsProvider.bookings.isEmpty) {
-              return NoBookingsScreen();
-            }
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Bookings'),
+      ),
+      body: FutureBuilder<Map<String, List<QueryDocumentSnapshot>>>(
+        future: fetchBookings(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData ||
+              (snapshot.data!['hotelBookings']!.isEmpty &&
+                  snapshot.data!['roomBookings']!.isEmpty)) {
+            return Center(child: Text('No bookings found.'));
+          } else {
+            List<QueryDocumentSnapshot> hotelBookings =
+                snapshot.data!['hotelBookings']!;
+            List<QueryDocumentSnapshot> roomBookings =
+                snapshot.data!['roomBookings']!;
+            double totalAmount =
+                calculateTotalAmount(hotelBookings, roomBookings);
             return Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Total Amount by Email:',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
                 Expanded(
                   child: ListView.builder(
-                    itemCount: bookingsProvider.totalAmountByEmail.length,
+                    itemCount: hotelBookings.length + roomBookings.length,
                     itemBuilder: (context, index) {
-                      var entry = bookingsProvider.totalAmountByEmail.entries.elementAt(index);
-                      return TotalAmountCard(
-                        email: entry.key,
-                        totalAmount: entry.value,
-                        bookings: bookingsProvider.bookings
-                            .where((booking) => booking.bookedBy == entry.key)
-                            .toList(),
-                        mpesaService: mpesaService,
-                      );
+                      if (index < hotelBookings.length) {
+                        var booking =
+                            hotelBookings[index].data() as Map<String, dynamic>;
+                        return BookingCard(
+                          booking: booking,
+                          bookingId: hotelBookings[index].id,
+                          collection: 'hotelBookings',
+                          onDelete: () => _deleteBooking(
+                              hotelBookings[index].id, 'hotelBookings'),
+                        );
+                      } else {
+                        var booking = roomBookings[index - hotelBookings.length]
+                            .data() as Map<String, dynamic>;
+                        return RoomBookingCard(
+                          booking: booking,
+                          bookingId: roomBookings[index - hotelBookings.length].id,
+                          collection: 'roomBookings',
+                          onDelete: () => _deleteBooking(
+                              roomBookings[index - hotelBookings.length].id, 'roomBookings'),
+                        );
+                      }
                     },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text('Total Amount: KSH ${totalAmount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle pay now action
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Pay Now button pressed')));
+                        },
+                        child: Text('Pay Now'),
+                      ),
+                    ],
                   ),
                 ),
               ],
             );
-          },
-        ),
+          }
+        },
       ),
     );
   }
+
+  double calculateTotalAmount(
+      List<QueryDocumentSnapshot> hotelBookings,
+      List<QueryDocumentSnapshot> roomBookings) {
+    double totalAmount = 0.0;
+
+    for (var booking in hotelBookings) {
+      var data = booking.data() as Map<String, dynamic>;
+      if (data.containsKey('amount')) {
+        totalAmount += data['amount'] ?? 0.0;
+      }
+    }
+
+    for (var booking in roomBookings) {
+      var data = booking.data() as Map<String, dynamic>;
+      if (data.containsKey('amount')) {
+        totalAmount += data['amount'] ?? 0.0;
+      }
+    }
+
+    return totalAmount;
+  }
+
+  Future<Map<String, List<QueryDocumentSnapshot>>> fetchBookings() async {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      throw Exception('No user logged in');
+    }
+
+    QuerySnapshot hotelBookingsSnapshot = await FirebaseFirestore.instance
+        .collection('hotelBookings')
+        .where('userId', isEqualTo: user.uid)
+        .get();
+
+    QuerySnapshot roomBookingsSnapshot = await FirebaseFirestore.instance
+        .collection('roomBookings')
+        .where('userId', isEqualTo: user.uid)
+        .get();
+
+    return {
+      'hotelBookings': hotelBookingsSnapshot.docs,
+      'roomBookings': roomBookingsSnapshot.docs,
+    };
+  }
+
+  Future<void> _deleteBooking(String bookingId, String collection) async {
+    try {
+      await FirebaseFirestore.instance.collection(collection).doc(bookingId).delete();
+    } catch (e) {
+      print('Error deleting booking: $e');
+    }
+  }
 }
 
-class TotalAmountCard extends StatelessWidget {
-  final String email;
-  final double totalAmount;
-  final List<Booking> bookings;
-  final MpesaService mpesaService;
+class BookingCard extends StatelessWidget {
+  final Map<String, dynamic>? booking;
+  final String bookingId;
+  final String collection;
+  final VoidCallback onDelete;
 
-  const TotalAmountCard({
-    required this.email,
-    required this.totalAmount,
-    required this.bookings,
-    required this.mpesaService,
+  BookingCard({
+    required this.booking,
+    required this.bookingId,
+    required this.collection,
+    required this.onDelete,
   });
 
   @override
@@ -91,88 +160,72 @@ class TotalAmountCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Email: $email',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
+            Text('Hotel: ${booking?['hotelName'] ?? 'Unknown'}',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             SizedBox(height: 8.0),
-            Text('Total Amount: $totalAmount'),
-            ...bookings.map((booking) {
-              final dateTime = booking.timestamp.toDate();
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 8.0),
-                  Text('Room Type: ${booking.roomType}'),
-                  Text('Rate: ${booking.rate}'),
-                  Text('Timestamp: $dateTime'),
-                ],
-              );
-            }).toList(),
+            Text('Amount: KSH ${booking?['amount'] ?? 'Unknown'}'),
+            Text('Display Name: ${booking?['displayName'] ?? 'Unknown'}'),
+            Text('Email: ${booking?['email'] ?? 'Unknown'}'),
+            Text('Phone: ${booking?['phoneNumber'] ?? 'Unknown'}'),
+            Text('Timestamp: ${booking?['timestamp']?.toDate() ?? 'Unknown'}'),
             SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: () async {
-                await _showPaymentDialog(context, totalAmount.toString());
-              },
-              child: Text('Pay Now'),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(Icons.remove_circle, color: Colors.red),
+                onPressed: onDelete,
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
 
-  Future<void> _showPaymentDialog(BuildContext context, String amount) async {
-    TextEditingController phoneNumberController = TextEditingController();
+class RoomBookingCard extends StatelessWidget {
+  final Map<String, dynamic>? booking;
+  final String bookingId;
+  final String collection;
+  final VoidCallback onDelete;
 
-    await showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Enter Payment Details'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: phoneNumberController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(labelText: 'Phone Number'),
+  RoomBookingCard({
+    required this.booking,
+    required this.bookingId,
+    required this.collection,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Hotel: ${booking?['hotelName'] ?? 'Unknown'}',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Text('Room: ${booking?['roomName'] ?? 'Unknown'}',
+                style: TextStyle(fontSize: 16)),
+            SizedBox(height: 8.0),
+            Text('Amount: KSH ${booking?['amount'] ?? 'Unknown'}'),
+            Text('Display Name: ${booking?['displayName'] ?? 'Unknown'}'),
+            Text('Email: ${booking?['email'] ?? 'Unknown'}'),
+            Text('Phone: ${booking?['phoneNumber'] ?? 'Unknown'}'),
+            Text('Timestamp: ${booking?['timestamp']?.toDate() ?? 'Unknown'}'),
+            SizedBox(height: 8.0),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(Icons.remove_circle, color: Colors.red),
+                onPressed: onDelete,
               ),
-              SizedBox(height: 16.0),
-              Text('Amount: $amount'),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                String phoneNumber = phoneNumberController.text;
-                _initiatePayment(context, phoneNumber, double.parse(amount));
-              },
-              child: Text('Pay'),
             ),
           ],
-        );
-      },
+        ),
+      ),
     );
-  }
-
-  Future<void> _initiatePayment(BuildContext context, String phoneNumber, double amount) async {
-    // Implement your payment initiation logic here
-    try {
-      // Example code to initiate payment
-      // Replace with your actual payment initiation code
-      print('Payment initiated for $amount to $phoneNumber');
-      Navigator.pop(context); // Close the dialog after successful payment
-      // Show success message or navigate to a success screen
-    } catch (e) {
-      // Handle payment failure
-      print('Error initiating payment: $e');
-    }
   }
 }
